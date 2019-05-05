f5e58be8159788074536ee974539479bb0c072ab
Taylor Morgan <taylor.morgan@gmail.com>
2019-05-05 19:36:36 -0400

    removing wss in favor of ws

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import VueMqtt from 'vue-mqtt';
 
 
-Vue.use(VueMqtt, 'wss://broker.losant.com:80', {
+Vue.use(VueMqtt, 'ws://broker.losant.com:80', {
   clientId: '5ccb82cf04156e0008b023b9',
   username: 'f0e3144c-5726-411b-8323-fc891163c225',
   password: 'da1096b88c72093595f94fb54aa2b013f368d66cc482b36f71f2082950fc1d35'
